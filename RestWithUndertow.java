e524cb9de1a838a30bcd53ee5b843de3c3cb816f
Kenta Kosugi <kenta.kosugi@gmail.com>
2019-12-20 19:03:53 +0900

    first commit

---
+++ RestWithUndertow.java
@@ -0,0 +1,19 @@
+import org.apache.camel.Exchange;
+
+public class RestWithUndertow extends org.apache.camel.builder.RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        restConfiguration()
+            .component("undertow")
+            .host("0.0.0.0")
+            .port("8080");
+rest()
+            .get("/hello")
+            .to("direct:hello");
+
+        from("direct:hello")
+            //.setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+            .transform().simple("{message: hello}");
+    }
+}
